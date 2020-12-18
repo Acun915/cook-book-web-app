643634c175a0cfd59a426fb54cd34f82c4952cf4
Przemyslaw <przemoacun@gmail.com>
2020-12-18 12:27:26 +0100

    database-schema drawing added, CommentEntity and RecipeEntity created

--- cook-book-spring/src/main/java/com/cookbook/entities/Comment.java
+++ cook-book-spring/src/main/java/com/cookbook/entities/Comment.java
@@ -1,2 +1,15 @@
-package com.cookbook.entities;public class Comment {
+package com.cookbook.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
 }
--- cook-book-spring/src/main/java/com/cookbook/entities/RecipeEntity.java
+++ cook-book-spring/src/main/java/com/cookbook/entities/RecipeEntity.java
@@ -1,2 +1,20 @@
-package com.cookbook.entities;public class RecipeEntity {
+package com.cookbook.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+public class RecipeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String ingredients;
+    private String content;
+    private List<Comment> comments;
 }
