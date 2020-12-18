1c35fe95255da048d72ccc1e0edf2143b1241d81
Przemyslaw <przemoacun@gmail.com>
2020-12-18 14:17:49 +0100

    CommentEntity, RecipeEntity: updated with new constructors

--- cook-book-spring/src/main/java/com/cookbook/entities/CommentEntity.java
+++ cook-book-spring/src/main/java/com/cookbook/entities/CommentEntity.java
@@ -1,11 +1,20 @@
 package com.cookbook.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class CommentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
+
+    public CommentEntity(String content) {
+        this.content = content;
+    }
 }
--- cook-book-spring/src/main/java/com/cookbook/entities/RecipeEntity.java
+++ cook-book-spring/src/main/java/com/cookbook/entities/RecipeEntity.java
@@ -1,9 +1,15 @@
 package com.cookbook.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class RecipeEntity {
 
     @Id
@@ -14,5 +20,19 @@ public class RecipeEntity {
     private String content;
     @OneToMany
     @JoinColumn(name = "recipe_id")
-    private List<CommentEntity> commentEntities;
+    private List<CommentEntity> comments;
+
+    public RecipeEntity(String title, String ingredients, String content, List<CommentEntity> comments){
+        this.title=title;
+        this.ingredients=ingredients;
+        this.content=content;
+        this.comments=comments;
+    }
+
+    public RecipeEntity(String title, String ingredients, String content){
+        this.title=title;
+        this.ingredients=ingredients;
+        this.content=content;
+        this.comments= new ArrayList<>();
+    }
 }
